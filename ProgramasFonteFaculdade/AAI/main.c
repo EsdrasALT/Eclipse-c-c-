894922e2fde16e21463434a8c6b36b122d5522e7
EsdrasALT <esdras.a.pessoal@gmail.com>
2024-05-30 11:24:43 -0300

    atualizacaoDeArquivos

---
+++ ProgramasFonteFaculdade/AAI/main.c
@@ -0,0 +1,11 @@
+//AAI
+#include "biblioteca.h"
+
+int main (void){
+	setbuf(stdout,NULL);
+
+    menuPrincipal();
+
+	return 0;
+}
+
---
+++ ProgramasFonteFaculdade/Aulas/listasEncadeadas/main.c
@@ -0,0 +1,13 @@
+#include "biblioteca.h"
+
+int main (void){
+	setbuf(stdout,NULL);
+    listaSimples *lista = NULL;
+    
+    for (int i=0; i<=3; i++){
+        lista = inserirInicio(lista);
+    }    
+    
+	return 0;
+}
+
