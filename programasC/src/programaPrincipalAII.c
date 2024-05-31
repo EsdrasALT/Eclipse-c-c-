9fdf27e67b4ac647449ffe70e13ad055eec900ad
EsdrasALT <esdras.a.pessoal@gmail.com>
2024-05-30 22:18:17 -0300

    atualização AAI

--- programasC/src/programaPrincipalAII.c
+++ programasC/src/programaPrincipalAII.c
@@ -9,12 +9,20 @@
 int main(void) {
 	setbuf(stdout, NULL);
 
-	//DataNascimento dataNascimento;
-	//dataNascimento = solicitarDataDeNascimento();
-	CadastroClientes *Clientes;
-	receberNomePreenchido(Clientes);
+//	DataNascimento dataNascimento;
+//	dataNascimento = solicitarDataDeNascimento();
 
-	//menuPrincipal();
+//	Clientes *clientes;
+//	receberNomePreenchido(clientes);
+
+//	Clientes cliente;
+//	inicializarCliente(&cliente);
+//	adicionarCarro(&cliente, "ABC1234", 1999);
+//	adicionarCarro(&cliente, "DEF5678", 2005);
+//	adicionarCarro(&cliente, "GHI9012", 2010);
+//    exibirCliente(&cliente);
+
+//	menuPrincipal();
 
 
 	return EXIT_SUCCESS;
