4bff5514fb14c7f7bc49b142b66f431f06245624
EsdrasALT <esdras.a.pessoal@gmail.com>
2024-06-09 22:38:36 -0300

    fiz caca tive que resetar arquivos

---
+++ executavelC/src/programaPrincipalAII.c
@@ -0,0 +1,16 @@
+// MAIN - programaPrincipal.c = https://codeshare.io/jALdkP
+
+#include "biblioteca.h"
+
+//Daniel Ribeiro Costa
+//Danilo Augusto
+//Esdras Altivo
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	menuPrincipal();
+
+
+	return EXIT_SUCCESS;
+}
